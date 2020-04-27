15ee9175cd1524b8d1647f4be11ef81f3c08ff86
keith <ko.keith@hotmail.com>
2020-04-27 12:08:13 -0400

    start solve method, create checkCell method

--- src/Solver.java
+++ src/Solver.java
@@ -1,5 +1,5 @@
 /**
- * Takes in a board and returns a solved board
+ * Takes in a board and returns a solved board. 0 Represents empty cell
  */
 public class Solver {
 
@@ -19,8 +19,22 @@ public class Solver {
         //print initial board
         printGrid();
 
-        
+        // iterate through each cell in the sudoku board
+        for(int row = 0; row < grid.length; row++){
+            for(int col = 0; col < grid.length; col++){
+                // If the current cell is 0, then look for valid option
+                if(grid[row][col] == 0){
 
+                }
+
+            }
+        }
+
+    }
+
+    // Given the grid, index of current cell, and possible cell num, check if it is valid according
+    // the 3 sudoku constraints
+    public void checkCell(int[][] grid,  int row, int col, int num){
 
     }
 
